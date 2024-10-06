69e0357f63baaf0e8a93192e33f9d69659fab107
Skyler <dare.skyler@gmail.com>
2024-10-06 13:00:21 -0400

    Create main.dart
    created login view

---
+++ main.dart
@@ -0,0 +1,156 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Instagram Clone App',
+      theme: ThemeData(
+        scaffoldBackgroundColor: const Color.fromARGB(255, 18, 25, 33),
+      ),
+      home: LoginScreen(),
+    );
+  }
+}
+
+class LoginScreen extends StatelessWidget {
+  final TextEditingController _usernameController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+
+  LoginScreen({super.key});
+
+  void _login() {
+
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Log in'),
+        centerTitle: true,
+        backgroundColor: const Color.fromRGBO(18, 25, 33, 1),
+        foregroundColor: Colors.white,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            TextField(
+              controller: _usernameController,
+              decoration: InputDecoration(
+                labelText: 'Email or Username',
+                labelStyle: const TextStyle(color:  Color.fromRGBO(148, 173, 199, 1)),
+                filled: true,
+                fillColor: const Color.fromRGBO(36, 54, 71, 1),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10.0),
+                  borderSide: BorderSide.none,
+                ),
+              ),
+            ),
+            const SizedBox(height: 10.0),
+            TextField(
+              controller: _passwordController,
+              decoration: InputDecoration(
+                labelText: 'Password',
+                labelStyle: const TextStyle(color:  Color.fromRGBO(148, 173, 199, 1)),
+                filled: true,
+                fillColor: const Color.fromRGBO(36, 54, 71, 1),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10.0),
+                  borderSide: BorderSide.none,
+                ),
+              ),
+              obscureText: true,
+            ),
+            const SizedBox(height: 10.0),
+            ElevatedButton(
+              onPressed: _login,
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.blue,
+                foregroundColor: Colors.white,
+              ),
+              child: const Text('Login'),
+            ),
+            const SizedBox(height: 10.0),
+
+            Align(
+              alignment: Alignment.centerLeft,
+              child: Material(
+                color: Colors.transparent,
+                child: InkWell(
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => ForgotPasswordScreen()),
+                    );
+                  },
+                  child: const Text('Forgot your password?',
+                    style: TextStyle(
+                      color: Color.fromRGBO(148, 173, 199, 1),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+
+            const SizedBox(height: 10.0),
+            Material(
+              color: Colors.transparent,
+              child: InkWell(
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => SignUpScreen()),
+                  );
+                },
+                child: const Text("Don't have an account? Sign Up.",
+                  style: TextStyle(
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class ForgotPasswordScreen extends StatelessWidget{
+  const ForgotPasswordScreen({super.key});
+
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(appBar: AppBar(
+        title: const Text('Forgot Password'),
+        centerTitle: true,
+        backgroundColor: const Color.fromRGBO(18, 25, 33, 1),
+        foregroundColor: Colors.white,
+      ),
+    );
+  }
+}
+
+class SignUpScreen extends StatelessWidget{
+  const SignUpScreen({super.key});
+
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(appBar: AppBar(
+        title: const Text('Sign Up'),
+        centerTitle: true,
+        backgroundColor: const Color.fromRGBO(18, 25, 33, 1),
+        foregroundColor: Colors.white,
+      ),
+      );
+  }
+}
