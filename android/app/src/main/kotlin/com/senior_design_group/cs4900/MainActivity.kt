0c9f26cbc1c1a2c39b9d46483f718231c604f070
Jadon <jadonzufall@gmail.com>
2024-10-07 17:14:49 -0400

    Project structure change

---
+++ android/app/src/main/kotlin/com/senior_design_group/cs4900/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.senior_design_group.cs4900
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,9 @@ import 'package:flutter/material.dart';
 
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'lib/firebase_options.dart';
+import 'firebase_options.dart';
 
-import 'auth.dart';
+import '../auth.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
