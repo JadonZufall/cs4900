1ac33a48168975ac57c9b0caf1587ea29d9d03cc
Jadon <jadonzufall@gmail.com>
2024-10-08 16:50:41 -0400

    database models

--- lib/database.dart
+++ lib/database.dart
@@ -2,13 +2,41 @@ import 'dart:developer';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
-class DatabaseInterface {
-  static final FirebaseFirestore db = FirebaseFirestore.instance;
+final FirebaseFirestore db = FirebaseFirestore.instance;
 
-  void create_user(String uid) async {
-    DatabaseInterface.db.collection("users").add(
-      Map<String, String>()
-    );
+class User {
+  static void create(String id) {
+
+    final user = <String, dynamic> {
+      "id": id,
+    };
+
+    db.collection("Users").add(user);
+  }
+
+  static void delete(String id) {
+    final ref = db.doc("users/$id");
+    ref.delete();
   }
 }
 
+class Post {
+  static void create(String id) {
+
+  }
+
+  static void delete(String id) {
+
+  }
+
+}
+
+class Comment {
+  static void create(String id) {
+
+  }
+
+  static void delete(String id) {
+
+  }
+}
