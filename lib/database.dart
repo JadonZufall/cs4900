8a92ba865c6156d0bb205377b52894edc61d83fa
Jadon <jadonzufall@gmail.com>
2024-10-08 16:59:33 -0400

    added model function purge

--- lib/database.dart
+++ lib/database.dart
@@ -5,10 +5,16 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 final FirebaseFirestore db = FirebaseFirestore.instance;
 
 class User {
-  static void create(String id) {
-
+  /* Represents a single user instance.
+  */
+  static void create(String id, String email, String username, String phone) {
     final user = <String, dynamic> {
       "id": id,
+      "email": email,
+      "username": username,
+      "phone": phone,
+      "followers": [],
+      "following": []
     };
 
     db.collection("Users").add(user);
@@ -18,25 +24,58 @@ class User {
     final ref = db.doc("users/$id");
     ref.delete();
   }
+
+  static void purge(String id) {
+    // Removes all content relating to the user.
+  }
 }
 
 class Post {
+  /* Represents a single upload.
+   */
   static void create(String id) {
 
   }
 
   static void delete(String id) {
+
+  }
+
+  static void purge(String id) {
 
   }
 
 }
 
 class Comment {
+  /* Represents a comment on a post.
+   */
   static void create(String id) {
 
   }
 
   static void delete(String id) {
+
+  }
+
+  static void purge(String id) {
+
+  }
+}
+
+class Conversation {
+  /* Represents a conversation between two users.
+   */
+
+  static void create(String id) {
+
+  }
+
+  static void delete(String id) {
+
+  }
+
+  static void purge(String id) {
 
   }
 }
