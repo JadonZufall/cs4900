5e6034896e9f8f6b82390060748c52f3223cbb27
Skyler <dare.skyler@gmail.com>
2024-10-21 22:05:06 -0400

    added some custom filters
    still need to iron out the UI

--- lib/views/camera/edit.dart
+++ lib/views/camera/edit.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:path/path.dart';
 import 'package:photofilters/photofilters.dart';
@@ -9,26 +10,46 @@ import 'dart:async';
 import 'dart:typed_data';
 
 import 'package:cs4900/main.dart';
+import 'filters.dart';
 
 class EditScreen extends StatelessWidget {
   final String imagePath;
 
   EditScreen({super.key, required this.imagePath});
 
-  
+  final List<List<double>> filters = [GREYSCALE, GALAXY, AGED];
+
   @override
   Widget build(BuildContext context) {
+    final Size size = MediaQuery.of(context).size;
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Apply Filters'), 
+        title: const Text('Apply Filters'),
         backgroundColor: const Color.fromRGBO(18, 25, 33, 1),
         foregroundColor: Colors.white,
       ),
-      body: Column(
-        children: [
-          Image.file(File(imagePath)),
-        ],
+      backgroundColor: Colors.black,
+      body: Center(
+        child: Container(
+          constraints: BoxConstraints(
+            maxWidth: size.width,
+            maxHeight: size.width,
+          ),
+          child: PageView.builder(
+              itemCount: filters.length,
+              itemBuilder: (context, index) {
+                return ColorFiltered(
+                  colorFilter: ColorFilter.matrix(filters[index]),
+                  child: Image.file(File(imagePath),
+                      width: 600, fit: BoxFit.cover),
+                );
+              }),
+        ),
       ),
     );
   }
 }
+
+
+
+// Image.file(File(imagePath)),
