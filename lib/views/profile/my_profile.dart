28acd47486fc35e53d8143a88d0e7ee7fbb1f0d9
Jadon <jadonzufall@gmail.com>
2024-10-25 04:08:30 -0400

    updated my_profiel page

--- lib/views/profile/my_profile.dart
+++ lib/views/profile/my_profile.dart
@@ -20,7 +20,7 @@ class MyProfileScreenState extends State<MyProfileScreen> {
   late UserInformation userInformation = UserInformation(FirebaseAuth.instance.currentUser!.uid);
 
 
-  void _settingsButton() {
+  void _editProfileButton() {
     navigatorKey.currentState?.pushNamed(RouteNames.myProfileSettingsRoute);
   }
 
@@ -44,7 +44,7 @@ class MyProfileScreenState extends State<MyProfileScreen> {
     );
     TextStyle bioTextStyle = const TextStyle(
       color: Colors.white,
-      fontSize: 24,
+      fontSize: 12,
       fontWeight: FontWeight.bold,
     );
 
@@ -73,22 +73,149 @@ class MyProfileScreenState extends State<MyProfileScreen> {
       future: userInformation.getProfilePicture(),
       builder: (context, snapshot) {
         if (snapshot.connectionState == ConnectionState.done) {
-          return Image.network("${snapshot.data}");
+          return CircleAvatar(
+              radius: 40,
+              backgroundImage: NetworkImage(snapshot.data ?? ""),
+          );
         }
-        else { return loadingIndicator; }
+        else {
+          return loadingIndicator;
+        }
       }
     );
+    FutureBuilder<List<Map<String, dynamic>>> uploadedPicturesField = FutureBuilder<List<Map<String, dynamic>>> (
+      future: userInformation.getUploadedImages(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.done) {
+          List<Image> images = [];
+
+          for (var i = 0; i < snapshot.data!.length; i++) {
+            Map<String, dynamic>? pair = snapshot.data?.elementAt(i);
+            String url = pair?["url"];
+            images.add(Image.network(
+              url,
+              fit: BoxFit.cover,
+            ));
+          }
+
+          return Expanded(
+            child: Container(
+              color: const Color.fromRGBO(18, 25, 33, 1),
+              child: GridView.builder(
+                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 3, childAspectRatio: 1),
+                  itemCount: images.length,
+                  itemBuilder: (context, index) {
+
+                    return Padding(
+                      padding: const EdgeInsets.all(2.0),
+                      child: images.elementAt(index),
+                    );
+                  }
+              ),
+            )
+          );
+        }
+        else {
+          return loadingIndicator;
+        }
+      },
+    );
+
+
+    Column body = Column(
+      children: [
+        Container(
+          color: const Color.fromRGBO(18, 25, 33, 1),
+          padding: const EdgeInsets.all(16.0),
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              profilePictureField,
+              const SizedBox(width: 16.0),
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    usernameField,
+                    const SizedBox(height: 4.0),
+                    bioField,
+                    const SizedBox(height: 8.0),
+                    // -------------------------------------------------------
+                    // This is all filler profile stats information that will need to be replaced with proper user data from firebase
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        _buildStatColumn('Posts', 0),
+                        _buildStatColumn('Followers', 0),
+                        _buildStatColumn('Following', 0),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+        Container(
+          color: const Color.fromRGBO(18, 25, 33, 1),
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                _buildActionButton('Edit Profile', _editProfileButton),
+              ],
+            ),
+          ),
+        ),
+        uploadedPicturesField,
+      ],
+    );
+
 
     return Scaffold(
       appBar: AppBar(title: const Text("My Profile")),
-      body: Column(
-        children: [
-          usernameField,
-          bioField,
-          profilePictureField,
-        ],
+      body: body,
+      floatingActionButton: FloatingActionButton(onPressed: _editProfileButton, child: const Icon(Icons.settings)),
+    );
+  }
+
+
+  Widget _buildStatColumn(String label, int count) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Text(
+          count.toString(), // converts int data for followers / posts / following to string
+          style: const TextStyle(
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+            color: Colors.white,
+          ),
+        ),
+        const SizedBox(height: 4.0),
+        Text(
+          label,
+          style: const TextStyle(
+              fontSize: 14,
+              color: Colors.white
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _buildActionButton(String title, VoidCallback onPressed) {
+    return ElevatedButton(
+      onPressed: onPressed,
+      style: ElevatedButton.styleFrom(
+        padding: const EdgeInsets.symmetric(horizontal: 16.0),
+        backgroundColor: const Color.fromRGBO(32, 49, 68, 1),
       ),
-      floatingActionButton: FloatingActionButton(onPressed: _settingsButton, child: const Icon(Icons.settings)),
+      child: Text(
+          title,
+          style: const TextStyle(color: Colors.white)
+      ),
     );
   }
 }
