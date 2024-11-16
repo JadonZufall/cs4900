ddd21367a29d81cd574dd6b9007bcae7000fcbc6
czehner1027 <ckm3320@wmich.edu>
2024-11-15 22:52:24 -0500

    Changed functions to use typescript

--- functions/index.js
+++ functions/index.js
@@ -1,19 +1,20 @@
-/**
- * Import function triggers from their respective submodules:
- *
- * const {onCall} = require("firebase-functions/v2/https");
- * const {onDocumentWritten} = require("firebase-functions/v2/firestore");
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
+const functions = require("firebase-functions");
+const admin = require("firebase-admin");
 
-const {onRequest} = require("firebase-functions/v2/https");
-const logger = require("firebase-functions/logger");
+// const {onRequest} = require("firebase-functions/v2/https");
+// const logger = require("firebase-functions/logger");
 
-// Create and deploy your first functions
-// https://firebase.google.com/docs/functions/get-started
+admin.initializeApp();
 
-// exports.helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+import {
+    onDocumentWritten,
+    onDocumentCreated,
+    onDocumentUpdated,
+    onDocumentDeleted,
+    Change,
+    FirestoreEvent
+} from "firebase-functions/v2/firestore";
+
+exports.myfunction = onDocumentUpdated("Notifications/{docId}", (event) => {
+    console.log(docId);
+});
