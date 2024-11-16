d9e2340cc0a07adacf807572ac01877581f86222
czehner1027 <ckm3320@wmich.edu>
2024-11-15 23:04:49 -0500

    Revert "Changed functions to use typescript"
    This reverts commit ddd21367a29d81cd574dd6b9007bcae7000fcbc6.

--- functions/index.js
+++ functions/index.js
@@ -1,20 +1,19 @@
-const functions = require("firebase-functions");
-const admin = require("firebase-admin");
+/**
+ * Import function triggers from their respective submodules:
+ *
+ * const {onCall} = require("firebase-functions/v2/https");
+ * const {onDocumentWritten} = require("firebase-functions/v2/firestore");
+ *
+ * See a full list of supported triggers at https://firebase.google.com/docs/functions
+ */
 
-// const {onRequest} = require("firebase-functions/v2/https");
-// const logger = require("firebase-functions/logger");
+const {onRequest} = require("firebase-functions/v2/https");
+const logger = require("firebase-functions/logger");
 
-admin.initializeApp();
+// Create and deploy your first functions
+// https://firebase.google.com/docs/functions/get-started
 
-import {
-    onDocumentWritten,
-    onDocumentCreated,
-    onDocumentUpdated,
-    onDocumentDeleted,
-    Change,
-    FirestoreEvent
-} from "firebase-functions/v2/firestore";
-
-exports.myfunction = onDocumentUpdated("Notifications/{docId}", (event) => {
-    console.log(docId);
-});
+// exports.helloWorld = onRequest((request, response) => {
+//   logger.info("Hello logs!", {structuredData: true});
+//   response.send("Hello from Firebase!");
+// });
